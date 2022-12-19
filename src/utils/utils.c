dba40934acce956c76e41b7ed937423be4a68883
twang <twang@student.42lyon.fr>
2022-12-19 19:30:36 +0100

    merge utils/ and src/utils

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -3,19 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 15:44:57 by wangthea          #+#    #+#             */
-/*   Updated: 2022/12/15 18:40:33 by wangthea         ###   ########.fr       */
+/*   Updated: 2022/12/19 19:29:39 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	ft_error(void)
-{
-	exit(ft_putstr_fd("Error\n", 2));
-}
 
 int	find_lowest_nb(t_stack *stack)
 {
